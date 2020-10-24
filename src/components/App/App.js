96f86dce0afff939f0890d72da60814fece41c7d
Joanna Baranowska <j.baranowska@hotmail.com>
2020-10-24 17:07:39 +0200

    Add Hero 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-//import List from '../List/List.js';
+import List from '../List/List.js';
 import styles from './App.scss';
-import Hero from '../Hero/Hero.js'
+//import Hero from '../Hero/Hero.js'
 
 class App extends React.Component {
   render() {
@@ -9,7 +9,7 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
-        <Hero className={styles.subtitle}></Hero>
+        <List />
       </main>
     )
   }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,7 @@ import styles from './Hero.scss';
 
 const Hero = () => (
 <header className={styles.component}>
-    <h2 className={styles.subtitle}>Things to do</h2>
+    <h2 className={styles.title}>Things to do</h2>
     <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" className={styles.image}/>
 </header>
 );
